f87ce29d9ba84622daf1ac6a3927e9353ed06655
bblovecc0816 <breeze5025@gmail.com>
2018-09-28 14:04:18 +0800

    Add files via upload

--- src/app/model/dp_allfailedtriggers_detail.ts
+++ src/app/model/dp_allfailedtriggers_detail.ts
@@ -1,6 +1,7 @@
 export class DPAllFailedTriggersDetail {
   InterfaceQueueName: string;
   TruckCenterID: number;
+  TruckCenter: string;
   RequestTypeID: number;
   PK: number;
   Parameter: number;
---
+++ src/app/model/dp_truckcenter.ts
@@ -0,0 +1,5 @@
+export class DPTruckCenter {
+  TruckCenterID: number;
+  TruckCenter: string;
+  Enviornment: string;
+}
