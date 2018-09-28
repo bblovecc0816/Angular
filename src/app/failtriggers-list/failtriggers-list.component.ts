750cf1b11c0b5ed583c6b119e82efbf617df31f7
bblovecc0816 <breeze5025@gmail.com>
2018-09-28 14:03:08 +0800

    Add files via upload

--- src/app/failtriggers-list/failtriggers-list.component.ts
+++ src/app/failtriggers-list/failtriggers-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DPAllFailedTriggersDetail } from '../model/dp_allfailedtriggers_detail';
+import { DPTruckCenter } from '../model/dp_truckcenter';
 import { TriggersService } from '../service/all-triggers-summary.service';
+
+import { Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-failtriggers-list',
@@ -10,14 +15,25 @@ import { TriggersService } from '../service/all-triggers-summary.service';
 export class FailtriggersListComponent implements OnInit {
   p: number = 1;
   failedTriggerList: DPAllFailedTriggersDetail[];
+  store: DPAllFailedTriggersDetail[];
+  // truckCenters$: Observable<DPTruckCenter[]>;
+  private searchTerms = new Subject<string>();
   constructor(private triggerService: TriggersService) { }
 
   ngOnInit() {
     this.getFailedTriggersList();
   }
+  search(term: string) {
+    if (term === '') {
+      this.failedTriggerList = this.store;
+    } else {
+      this.failedTriggerList = this.store.filter(f => f.TruckCenter.indexOf(term) >= 0);
+    }
+  }
   getFailedTriggersList(): void {
     this.triggerService.getFailedTriggersList()
       .subscribe(failedList => {this.failedTriggerList = failedList;
+      this.store = failedList;
       console.log(this.failedTriggerList); });
   }
 }
