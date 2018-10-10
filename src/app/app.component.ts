626d01707820f7265e654fd390039d9526aa17b9
bblovecc0816 <breeze5025@gmail.com>
2018-10-10 15:47:04 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dealer Point Intergation Log';
+  title = 'Dealer Point Integration Log';
 }
